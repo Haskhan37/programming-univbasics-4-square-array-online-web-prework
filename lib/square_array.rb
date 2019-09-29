16be445e00c683e71731dc978156dcd0f4f3fbec
Hasan Khan <hassankhankabacha@gmail.com>
2019-09-29 17:58:10 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,10 @@
-def square_array(array)
-  # your code here
+def array = [5,6,7,8]
+  square_array(array)
+  
+  #=> [25,36,49,64]
+  
+  new_numbers = [10,12,15,20]
+  
+  square_array(new_numbers)
+  #=> [100,144,225,400]
 end
